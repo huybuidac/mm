9ccc83e72d196acd8f935b96f2bdb00be820ead0
huybuidac <huybuidac@gmail.com>
2025-08-07 09:43:04 +0700

    fix: be

--- apps/nest/apps/api/src/main.ts
+++ apps/nest/apps/api/src/main.ts
@@ -7,33 +7,34 @@ import { promiseHelper } from '@app/helper/promise.helper'
 
 const localDev = process.env.LOCAL_DEV === 'true'
 
-if (localDev) {
-  async function bootstrap() {
-    const app = await NestFactory.create(ApiModule)
-    app.enableCors({})
-    await setupNestApp(app)
-    setupSwaggerUI(app)
-    await app.listen(process.env.port ?? 3000)
-    console.log(`Server is running at http://localhost:${process.env.port ?? 3000}/docs`)
-  }
-
-  bootstrap()
+// if (localDev) {
+async function bootstrap() {
+  const app = await NestFactory.create(ApiModule, {
+    cors: true,
+  })
+  await setupNestApp(app)
+  setupSwaggerUI(app)
+  await app.listen(process.env.port ?? 3000)
+  console.log(`Server is running at http://localhost:${process.env.port ?? 3000}/docs`)
 }
 
-let server: Handler
+bootstrap()
+// }
 
-export const handler = async (event: any, context: Context, callback) => {
-  const serverInited = !!server
-  server = server ?? (await bootstrapServerless(ApiModule, 'api'))
-  if (event.Warmup) {
-    console.log('Warmup event')
-    if (serverInited) {
-      // small delay to invoke all concurrency lambdas
-      await promiseHelper.delay(30)
-    }
-    return true
-  } else {
-    console.log('path=', event?.rawPath)
-    return server(event, context as any, callback) as any
-  }
-}
+// let server: Handler
+
+// export const handler = async (event: any, context: Context, callback) => {
+//   const serverInited = !!server
+//   server = server ?? (await bootstrapServerless(ApiModule, 'api'))
+//   if (event.Warmup) {
+//     console.log('Warmup event')
+//     if (serverInited) {
+//       // small delay to invoke all concurrency lambdas
+//       await promiseHelper.delay(30)
+//     }
+//     return true
+//   } else {
+//     console.log('path=', event?.rawPath)
+//     return server(event, context as any, callback) as any
+//   }
+// }
