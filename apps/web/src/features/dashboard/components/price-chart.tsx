cdedc14751f3d9b0742452caee7d2c99ded8be6e
huybuidac <huybuidac@gmail.com>
2025-08-07 14:33:33 +0700

    feat: update price

--- apps/web/src/features/dashboard/components/price-chart.tsx
+++ apps/web/src/features/dashboard/components/price-chart.tsx
@@ -1,6 +1,7 @@
 export function PriceChart(props: { tokenAddress: string; chainId: string }) {
-  const { tokenAddress, chainId } = props
-  const src = `https://www.dextools.io/widget-chart/en/abstract/pe-light/${tokenAddress}?theme=light&chartType=2&chartResolution=30&drawingToolbars=false`
+  const { chainId } = props
+
+  const src = `https://www.dextools.io/widget-chart/en/abstract/pe-light/0x4004d7893128c9e09b57c5e3708ff6516d517cbf?theme=light&chartType=2&chartResolution=30&drawingToolbars=false`
   return (
     <>
       {chainId === '2741' ? (
